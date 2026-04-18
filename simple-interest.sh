7f3915e2cac16af21a79331a5080814db3fa9d63
PAGARE LOKESH DNYANESHWAR <lokesh.pagare231@vit.edu>
2026-04-18 23:22:40 +0530

    Add simple interest shell script

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+echo "Enter principal:"
+read p
+echo "Enter rate of interest:"
+read r
+echo "Enter time period:"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+
+echo "Simple Interest = $si"  
